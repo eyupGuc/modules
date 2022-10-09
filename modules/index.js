21d7887a63369d712fbce5326936b0e8c1ac82e2
eyupGuc <meydanguresi@gmail.com>
2022-10-09 07:12:10 +0300

    finished

---
+++ modules/index.js
@@ -0,0 +1,21 @@
+console.log("JS is running")
+import karesi, {W,WORKING_HOURS_WEEK,dec,inc,sum,counter} from './mymodule.js';
+// import  {default as karesi,WHW,WORKING_HOURS_WEEK,dec,inc,sum,counter} from './mymodule.js';
+
+// console.log('W:>>', W);
+// console.log('WORKING_HOURS_WEEK:>>', WORKING_HOURS_WEEK)
+// console.log('karesi(4)', karesi(4))
+
+inc(4);
+inc(6);
+dec(
+
+)
+;
+console.log("dec() :>>" ,dec())
+inc()
+console.log('counter :>>', counter)
+//counter=29; module dışından değişken değiştirilemez let bile olsa.
+
+inc()
+console.log('counter :>>', counter)
---
+++ modules/module2.js
@@ -0,0 +1,4 @@
+console.log("module 2 is running")
+import {inc} from './mymodule.js'
+// burda inc bellekte 1 tane var o yüzde en son inc 10 du ve 100 eklediğimizde 110 oldu.
+console.log('inc(100) :>>', inc(100))
